b9fb92fb432b4645c0670414ebb345910feaa932
maddurivenkys <maddurivenkys@gmail.com>
2022-02-15 23:10:50 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,20 @@
-import { Component, VERSION , OnInit } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
 import { of, from } from 'rxjs';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
 
-  ngOnInit(){
-     of(2, 4, 6, 8).subscribe(console.log);
+  ngOnInit() {
+    of(2, 4, 6, 8).subscribe(console.log);
 
-     from([20, 15, 10 , 5]).subscribe(
-      item => console.log(`resulting item ... $(item)`),
-      err => console.error(`error occured ${err}`),
+    from([20, 15, 10, 5]).subscribe(
+      (item) => console.log(`resulting item ... $(item)`),
+      (err) => console.error(`error occured ${err}`),
       () => console.log('completed')
-       
-     );
+    );
   }
 }
